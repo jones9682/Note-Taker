f917b6f3f7d7d1ca6110d55e8724181ff0a17696
Bobby Jones Jr <jones9682@yahoo.com>
2020-08-06 12:54:24 -0400

    added require to routes file path

--- Develop/server.js
+++ Develop/server.js
@@ -1,36 +1,39 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+// Using the `fs` module
 const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let notes = [];
+//Require routes file
+require('./routes/routes')(app);
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+// Setup listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
 
-// * GET `/notes` - Should return the `notes.html` file.
 
-//   * GET `*` - Should return the `index.html` file
 
-// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes
 
-// using the `fs` module.
+
+
+
+
+
+
+
 // fs read and fs write.
 
 // * The following API routes should be created:
@@ -43,6 +46,3 @@ app.get("/notes", function (req, res) {
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
 //   This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, 
 //   you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
